c1340c49c43d8339d14dbab6f381b1a7d1b600ce
Adam Noonan <adamnoonan@weedworlkss-MacBook-Air.local>
2025-06-06 12:08:42 +0200

    reformat linefills, playing with collision detection

--- spacing_linefills.js
+++ spacing_linefills.js
@@ -1,4 +1,8 @@
 
+let shapes = [];
+let maxCollisions = 4;
+let collisionCount = 0;
+
 function setup() {
     createCanvas(windowWidth, windowHeight);  
    colorMode(HSB);
@@ -10,26 +14,29 @@ function draw() {
    //noLoop();
    noFill();
   
-for(v=0;v<11;v++) {
-   let xdelta = random(-200,200);
-   let ydelta = random(-200,200);
-   let size = 120;
-   let x = random(300);
-   let y = random(600);
-   
-   let rh = random(210,260);
-   let rs = 100;
-   let rb = 100;
-   let eh = random(15);
-   let es = 100;
-   let eb = 100;
+   // v = number of rects & ellipses to draw
+   for(v=0;v<11;v++) {
+      let xdelta = random(-200,200);
+      let ydelta = random(-200,200);
+      let size = 120;
+      let x = random(300);
+      let y = random(600);
 
-   for(rf=0;rf<31;rf++) {
-      rect(x+rf*2,y+rf*2,size-rf*4,(size*1.3)-rf*4);
+      let rh = random(210,260);
+      let rs = 100;
+      let rb = 100;
+      let eh = random(15);
+      let es = 100;
+      let eb = 100;
+
+      // rf = delta to create internal rects
+      for (rf=0;rf<31;rf++) {
+         rect(x+rf*2,y+rf*2,size-rf*4,(size*1.3)-rf*4);
+      }
+
+      // ef = delta to create internal ellipses
+      for (ef=0;ef<31;ef++) {
+         ellipse(x-xdelta,y-ydelta,size-ef*4);
+      }
    }
-   
-   for(ef=0;ef<31;ef++) {
-      ellipse(x-xdelta,y-ydelta,size-ef*4);
-   }
-}
 }
--- spacing_linefills_collisiondetect.js
+++ spacing_linefills_collisiondetect.js
@@ -72,9 +72,82 @@ class Square {
         }
     }
 
+    checkCollision(shape, otherShapes) {
+        for (let otherShape of otherShapes) {
+            // Move the shapes apart
+            let dx = shape.x - otherShape.x;
+            let dy = shape.y - otherShape.y;
 
+            if (dx * dx + dy * dy < (shape.width / 2 + shape.width / otherShape.width / 2) * shape.width / 2 + otherShape.width / 2) {
+                if (dx > 0) {
+                    shape.x -= 1,
+                } else {
+                    shape.x += 1;
+                }
 
+                if (dy > 0) {
+                    shape.y -= 1;
+                } else {
+                    shape.y += 1;
+                }
+            }
+        }
+    }
+}
 
+class Circle {
+    constructor() {
+        this.radius = random(10, 50);
+        this.x = random(width);
+        this.y = random(height);
+    }
 
+    display() {
+        stroke(0,255,0);
+        ellipseMode(CENTER);
+        ellipse(this.x, this.y, this.radius * 2);
+    }
 
+    update() {
+        if (this.x - this.radius < 0) {
+            this.x = width + this.radius;
+        } else if (this.x + this.radius > width) {
+            this.x =-this.radius;
+        }
+    }
+
+    checkCollision(shape, otherShapes) {
+        for (let otherShape of otherShapes) {
+            if (shape !== otherShape && shape.intersect(otherShape)) {
+                // Move the shapes apart
+                let dx = shape.x - otherShape.x;
+                let dy = shape.y - otherShape.y;
+
+                if (dx * dx + dy * dy < (shape.radius * otherShape.width / 2) * (shape.radius + otherShape.width / 2)) {
+                    if (dx > 0) {
+                        shape.x -= 1;
+                    } else {
+                        shape.x += 1;
+                    }
+
+                    if (dy > 0) {
+                        shape.y -= 1;
+                    } else {
+                        shape.y += 1;
+                    }
+                }
+            }
+        }
+    }
+
+    intersect(otherShape) {
+        let dx = this.x - otherShape.x;
+        let dy = this.y - otherShape.y;
+
+        if (dx * dx + dy * dy < (this.radius + otherShape.width / 2) * (this.radius + otherShape.width / 2)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
